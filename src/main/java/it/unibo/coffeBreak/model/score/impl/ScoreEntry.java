f53a015527c4e57f6119a33996eaacef99037f59
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-03-30 18:06:05 +0200

    docs: add Javadoc and refactor ScoreEntry methods

--- src/main/java/it/unibo/coffeBreak/model/score/impl/ScoreEntry.java
+++ src/main/java/it/unibo/coffeBreak/model/score/impl/ScoreEntry.java
@@ -5,11 +5,24 @@ import java.util.Objects;
 
 import it.unibo.coffeBreak.model.score.api.Entry;
 
+/**
+ * Concrete implementation of an {@link Entry} that represents a score entry
+ * with a player name and score value. This class is immutable and thread-safe.
+ */
 public class ScoreEntry implements Entry, Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private final String name;
     private final int score;
 
+    /**
+     * Creates a new ScoreEntry with the specified name and score.
+     * 
+     * @param name  the player name (cannot be null)
+     * @param score the score value
+     * @throws NullPointerException if name is null
+     */
     public ScoreEntry(final String name, final int score) {
         this.name = Objects.requireNonNull(name, "Name cannot be null");
         this.score = score;
@@ -25,30 +38,42 @@ public class ScoreEntry implements Entry, Serializable {
         return this.score;
     }
 
+    /**
+     * Compares this entry with another entry based on score (descending order).
+     * 
+     * @param o the entry to be compared
+     * @return a negative integer, zero, or a positive integer if this entry's score
+     *         is greater than, equal to, or less than the specified entry's score
+     * @throws NullPointerException if the specified entry is null
+     */
     @Override
-    public int compareTo(Entry o) {
+    public int compareTo(final Entry o) {
         return Integer.compare(Objects.requireNonNull(o).getScore(), this.score);
     }
 
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(final Object obj) {
         if (this == obj) {
             return true;
         }
-        if (obj == null || getClass() != obj.getClass()) {
+        if (obj == null || this.getClass() != obj.getClass()) {
             return false;
         }
-        ScoreEntry that = (ScoreEntry) obj;
-        return score == that.score && Objects.equals(name, that.name);
+        final ScoreEntry that = (ScoreEntry) obj;
+        return this.score == that.score && Objects.equals(this.name, that.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, score);
+        return Objects.hash(this.name, this.score);
     }
 
     @Override
     public String toString() {
-        return "ScoreEntry[name= " + this.name + ", " + "score= " + this.score + "]";
+        return new StringBuilder("ScoreEntry[")
+                .append("name=").append(this.name)
+                .append(", score=").append(this.score)
+                .append(']')
+                .toString();
     }
 }
