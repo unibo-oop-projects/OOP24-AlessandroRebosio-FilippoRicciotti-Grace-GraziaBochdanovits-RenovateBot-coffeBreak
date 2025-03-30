3a701b39faee70749431f260181a5e6ac9a7b726
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-03-30 13:53:15 +0200

    implemented GameBonus.java

---
+++ src/main/java/it/unibo/coffeBreak/model/score/impl/GameBonus.java
@@ -0,0 +1,34 @@
+package it.unibo.coffeBreak.model.score.impl;
+
+import it.unibo.coffeBreak.model.score.api.Bonus;
+
+public class GameBonus implements Bonus {
+
+    final static int AMOUNT = 100;
+    private int bonus;
+
+    public GameBonus() {
+        this.bonus = 0;
+    }
+
+    @Override
+    public int getBonus() {
+        return this.bonus;
+    }
+
+    @Override
+    public void setBonus(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("The value must be positive.");
+        }
+        this.bonus = value;
+    }
+
+    @Override
+    public void calculate() {
+        if (this.bonus > 0) {
+            this.bonus = this.bonus - AMOUNT;
+        }
+    }
+    
+}
