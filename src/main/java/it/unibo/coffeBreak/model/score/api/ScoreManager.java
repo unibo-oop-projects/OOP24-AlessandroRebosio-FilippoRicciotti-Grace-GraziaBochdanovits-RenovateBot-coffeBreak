1691d81de8c30b988bcf408f84da96da1b7d5e05
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-03-30 15:37:21 +0200

    added ScoreManager.java interface

---
+++ src/main/java/it/unibo/coffeBreak/model/score/api/ScoreManager.java
@@ -0,0 +1,21 @@
+package it.unibo.coffeBreak.model.score.api;
+
+import java.util.List;
+
+public interface ScoreManager<X> {
+    int getCurrentScore();
+
+    int getCurrentBonus();
+
+    List<X> getLeaderBoard();
+
+    int getHighestScore();
+
+    void calculateBonus();
+
+    void earnPoints(int amount);
+
+    void endMap();
+
+    void endGame();
+}
