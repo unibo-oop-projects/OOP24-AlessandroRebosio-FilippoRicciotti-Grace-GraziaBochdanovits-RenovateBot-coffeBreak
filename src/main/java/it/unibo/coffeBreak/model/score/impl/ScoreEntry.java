d6ad5cb2ad796ec88b0db01272758f752d762c4e
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-03-30 14:09:57 +0200

    implemented ScoreEntry.java

---
+++ src/main/java/it/unibo/coffeBreak/model/score/impl/ScoreEntry.java
@@ -0,0 +1,36 @@
+package it.unibo.coffeBreak.model.score.impl;
+
+import java.util.Objects;
+
+import it.unibo.coffeBreak.model.score.api.Entry;
+
+public class ScoreEntry implements Entry, Comparable<ScoreEntry> {
+
+    private final String name;
+    private final int score;
+
+    public ScoreEntry(final String name, final int score) {
+        this.name = Objects.requireNonNull(name, "Name cannot be null");
+        this.score = score;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getScore() {
+        return this.score;
+    }
+
+    @Override
+    public int compareTo(ScoreEntry o) {
+        return Integer.compare(Objects.requireNonNull(o).getScore(), this.score);
+    }
+
+    @Override
+    public String toString() {
+        return "ScoreEntry[name= " + this.name + ", " + "score= " + this.score + "]";
+    }
+}
