8a82c07a7e2044250c9777fb4bd090f232028c7f
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-03-30 18:30:44 +0200

    docs: add Javadoc and refactor ScoreRepository methods

--- src/main/java/it/unibo/coffeBreak/model/score/impl/ScoreRepository.java
+++ src/main/java/it/unibo/coffeBreak/model/score/impl/ScoreRepository.java
@@ -8,49 +8,88 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import it.unibo.coffeBreak.model.score.api.Entry;
 import it.unibo.coffeBreak.model.score.api.Repository;
 
+/**
+ * Implementation of {@link Repository} for storing and retrieving score
+ * entries.
+ * Uses Java serialization to persist data to a file in the user's home
+ * directory.
+ */
 public class ScoreRepository implements Repository<Entry> {
 
     private static final String FILE_NAME = "leaderBoard.ser";
     private final File dataFile;
 
+    /**
+     * Constructs a new ScoreRepository.
+     * The data file will be created in the user's home directory.
+     */
     public ScoreRepository() {
         this.dataFile = new File(System.getProperty("user.home"), FILE_NAME);
     }
 
+    /**
+     * Saves a list of entries to the repository.
+     *
+     * @param list the list of entries to be saved
+     * @return true if the operation was successful
+     * @throws RepositoryException if an error occurs during the save operation
+     */
     @Override
-    public boolean save(List<Entry> list) {
+    public boolean save(final List<Entry> list) {
+        Objects.requireNonNull(list);
+
         if (list.isEmpty()) {
             return true;
         }
 
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dataFile))) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(this.dataFile))) {
             oos.writeObject(list);
             return true;
-        } catch (IOException e) {
-            throw new RepositoryException("Errore durante il salvataggio dei dati", e);
+        } catch (final IOException e) {
+            throw new RepositoryException("Error while saving data", e);
         }
     }
 
+    /**
+     * Loads all entries from the repository.
+     *
+     * @return a list of all entries stored in the repository, or an empty list if
+     *         the repository is empty
+     * @throws RepositoryException if an error occurs during the load operation
+     */
     @Override
     @SuppressWarnings("unchecked")
     public List<Entry> load() {
-        if (!dataFile.exists()) {
+        if (!this.dataFile.exists()) {
             return Collections.emptyList();
         }
 
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dataFile))) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(this.dataFile))) {
             return (List<Entry>) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            throw new RepositoryException("Errore durante il caricamento dei dati", e);
+            throw new RepositoryException("Error while loading data", e);
         }
     }
 
+    /**
+     * Exception thrown when an error occurs in repository operations.
+     */
     public static class RepositoryException extends RuntimeException {
-        public RepositoryException(String message, Throwable cause) {
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * Constructs a new RepositoryException with the specified detail message and
+         * cause.
+         *
+         * @param message the detail message
+         * @param cause   the cause of the exception
+         */
+        public RepositoryException(final String message, final Throwable cause) {
             super(message, cause);
         }
     }
