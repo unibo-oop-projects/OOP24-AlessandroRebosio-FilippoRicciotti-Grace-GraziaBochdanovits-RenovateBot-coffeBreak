10531f805f9ab7767c25f6e3ed236c0326237f5d
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-03-30 18:20:54 +0200

    docs: add JavaDoc in Repository.java

--- src/main/java/it/unibo/coffeBreak/model/score/api/Repository.java
+++ src/main/java/it/unibo/coffeBreak/model/score/api/Repository.java
@@ -2,8 +2,27 @@ package it.unibo.coffeBreak.model.score.api;
 
 import java.util.List;
 
+/**
+ * Interface for a generic repository that handles saving and loading of
+ * elements.
+ *
+ * @param <X> the type of elements stored in the repository
+ */
 public interface Repository<X> {
+
+    /**
+     * Saves a list of elements to the repository.
+     *
+     * @param list the list of elements to be saved
+     * @return true if the operation was successful, false otherwise
+     */
     boolean save(List<X> list);
 
+    /**
+     * Loads all elements from the repository.
+     *
+     * @return a list of all elements stored in the repository
+     */
     List<X> load();
+
 }
