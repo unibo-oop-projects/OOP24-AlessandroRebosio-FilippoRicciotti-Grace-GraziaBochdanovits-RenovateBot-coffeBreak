64199a8b100ae59e8c36cc51ae1639f7a3307cd1
Grace <grazia.bochdanovits@studio.unibo.it>
2025-03-31 16:42:02 +0200

    fixed the javadoc of Entity.java and GameEntity.java

--- src/main/java/it/unibo/coffebreak/model/entity/api/Entity.java
+++ src/main/java/it/unibo/coffebreak/model/entity/api/Entity.java
@@ -1,7 +1,7 @@
 package it.unibo.coffebreak.model.entity.api;
 
-import it.unibo.coffebreak.model.entity.impl.Position;
-import it.unibo.coffebreak.model.entity.impl.Dimension;
+import it.unibo.coffebreak.model.utility.Dimension;
+import it.unibo.coffebreak.model.utility.Position;
 
 /**
  * Represents a bonus system that can be applied to a game.
@@ -10,23 +10,24 @@ import it.unibo.coffebreak.model.entity.impl.Dimension;
 public interface Entity {
 
     /**
-    * Retrieves the position of the entity.
-    *
-    * @return the current position of the entity.
-    */
-    public Position getPosition();
+     * Retrieves the position of the entity.
+     *
+     * @return the current position of the entity.
+     */
+    Position getPosition();
 
     /**
-    * Retrieves the dimensions of the entity.
-    *
-    * @return the current dimensions of the entity.
-    */
-    public Dimension getDimension();
+     * Retrieves the dimensions of the entity.
+     *
+     * @return the current dimensions of the entity.
+     */
+    Dimension getDimension();
 
     /**
-    * Updates the state of the entity based on the elapsed time.
-    *
-    * @param deltaTime the time elapsed since the last update, in milliseconds.
-    */
-    public abstract void update(long deltaTime);
+     * Updates the state of the entity based on the elapsed time.
+     *
+     * @param deltaTime the time elapsed since the
+     *                  last update, in milliseconds.
+     */
+    void update(long deltaTime);
 }
--- src/main/java/it/unibo/coffebreak/model/entity/impl/GameEntity.java
+++ src/main/java/it/unibo/coffebreak/model/entity/impl/GameEntity.java
@@ -1,24 +1,39 @@
 package it.unibo.coffebreak.model.entity.impl;
 
 import it.unibo.coffebreak.model.entity.api.Entity;
+import it.unibo.coffebreak.model.utility.Dimension;
+import it.unibo.coffebreak.model.utility.Position;
 
-/*
- * Implementation of {@link Entity} interface that manage the entities.
+/**
+ * Represents a base class for game entities that have a position and a dimension.
+ * This class implements the {@link Entity} interface and provides common functionality
+ * for managing the position and dimension of entities in the game.
  */
 public abstract class GameEntity implements Entity {
 
     /*
      * The position and dimention of the entities in the game.
      */
-    private Position position;
-    private Dimension dimension;
-    
+    private final Position position;
+    private final Dimension dimension;
+
+     /**
+     * Constructs a new {@link GameEntity} with the specified position and dimension.
+     *
+     * @param p the position of the entity.
+     * @param d the dimension of the entity.
+     */
+    public GameEntity(final Position p, final Dimension d) {
+        this.position = p;
+        this.dimension = d;
+    }
+
     /**
      * {@inheritDoc}
      */
     @Override
     public final Position getPosition() {
-        return this.position;
+        return new Position(this.position.getX(), this.position.getY());
     }
 
     /**
@@ -28,11 +43,4 @@ public abstract class GameEntity implements Entity {
     public final Dimension getDimension() {
         return this.dimension;
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public abstract void update(long deltaTime);
-
 }
