1e6241db796013808574879d58f3cf0ea9f3d23d
Grace <grazia.bochdanovits@studio.unibo.it>
2025-04-24 12:04:31 +0200

    refactor: remove position/dimension methods from Platform interface

--- src/main/java/it/unibo/coffebreak/model/api/entity/platform/Platform.java
+++ src/main/java/it/unibo/coffebreak/model/api/entity/platform/Platform.java
@@ -1,14 +1,11 @@
 package it.unibo.coffebreak.model.api.entity.platform;
 
-import it.unibo.coffebreak.model.impl.utility.Dimension;
-import it.unibo.coffebreak.model.impl.utility.Position;
-
 /**
  * Represents a platform in the game.
  * A platform can have different properties such as the ability to break and its friction level.
  */
 public interface Platform {
-   /**
+    /**
      * Determines if the platform can break during gameplay.
      * @return {@code true} if the platform is breakable, {@code false} otherwise
      */
@@ -20,21 +17,5 @@ public interface Platform {
      * @return the friction level as a float.
      */
     float getFriction();
-
-    /**
-     * Gets the current position of this platform in the game world coordinates.
-     *
-     * @return the platform's position as a {@link Position} object
-     * @see Position
-     */
-    Position getPlatformPosition();
-
-    /**
-     * Gets the dimensions of this platform.
-     *
-     * @return the platform's dimensions as a {@link Dimension} object
-     * @see Dimension
-     */
-    Dimension getPlatformDimension();
 
 }
