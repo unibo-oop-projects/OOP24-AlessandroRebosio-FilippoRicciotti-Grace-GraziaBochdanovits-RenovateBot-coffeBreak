70ef44831b1da6a21234e10a4071e31532b2db8a
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-04-27 13:03:52 +0200

    doc: added javaDoc in GameCollectibleFactoryTest

--- src/test/java/it/unibo/coffebreak/model/entity/item/GameCollectibleFactoryTest.java
+++ src/test/java/it/unibo/coffebreak/model/entity/item/GameCollectibleFactoryTest.java
@@ -11,18 +11,57 @@ import it.unibo.coffebreak.model.api.entity.item.Collectible;
 import it.unibo.coffebreak.model.impl.entity.item.GameCollectibleFactory;
 import it.unibo.coffebreak.model.impl.entity.item.ItemType;
 
+/**
+ * Test class for {@link GameCollectibleFactory}.
+ * <p>
+ * Verifies the correct creation of different types of collectible items
+ * with their expected properties and behaviors.
+ * </p>
+ * 
+ * <p>
+ * The tests cover:
+ * <ul>
+ * <li>Creation of all supported item types (hammer, coin, bag, hat)</li>
+ * <li>Verification of correct item type assignment</li>
+ * <li>Basic property validation</li>
+ * </ul>
+ * </p>
+ * 
+ * @see GameCollectibleFactory
+ * @see Collectible
+ * @see ItemType
+ * 
+ * @author Alessandro Rebosio
+ */
 class GameCollectibleFactoryTest {
 
+    /** Test X coordinate for item position. */
     private static final int X = 10;
+
+    /** Test Y coordinate for item position. */
     private static final int Y = 20;
 
+    /** Test width for item dimension. */
     private static final int WIDTH = 30;
+
+    /** Test height for item dimension. */
     private static final int HEIGTH = 40;
 
     private GameCollectibleFactory factory;
     private Position testPosition;
     private Dimension testDimension;
 
+    /**
+     * Initializes test fixtures before each test method.
+     * <p>
+     * Creates:
+     * <ul>
+     * <li>A new {@link GameCollectibleFactory} instance</li>
+     * <li>A standard test position</li>
+     * <li>A standard test dimension</li>
+     * </ul>
+     * </p>
+     */
     @BeforeEach
     void setUp() {
         factory = new GameCollectibleFactory();
@@ -30,6 +69,16 @@ class GameCollectibleFactoryTest {
         testDimension = new Dimension(WIDTH, HEIGTH);
     }
 
+    /**
+     * Tests the creation of a hammer item.
+     * <p>
+     * Verifies that:
+     * <ul>
+     * <li>The returned object is not null</li>
+     * <li>The item has the correct {@link ItemType#HAMMER} type</li>
+     * </ul>
+     * </p>
+     */
     @Test
     void testCreateHammer() {
         final Collectible hammer = factory.createHammer(testPosition, testDimension);
@@ -37,6 +86,16 @@ class GameCollectibleFactoryTest {
         assertEquals(ItemType.HAMMER, hammer.getType());
     }
 
+    /**
+     * Tests the creation of a coin item.
+     * <p>
+     * Verifies that:
+     * <ul>
+     * <li>The returned object is not null</li>
+     * <li>The item has the correct {@link ItemType#COIN} type</li>
+     * </ul>
+     * </p>
+     */
     @Test
     void testCreateCoin() {
         final Collectible coin = factory.createCoin(testPosition, testDimension);
@@ -44,6 +103,16 @@ class GameCollectibleFactoryTest {
         assertEquals(ItemType.COIN, coin.getType());
     }
 
+    /**
+     * Tests the creation of a bag item.
+     * <p>
+     * Verifies that:
+     * <ul>
+     * <li>The returned object is not null</li>
+     * <li>The item has the correct {@link ItemType#BAG} type</li>
+     * </ul>
+     * </p>
+     */
     @Test
     void testCreateBag() {
         final Collectible bag = factory.createBag(testPosition, testDimension);
@@ -51,11 +120,20 @@ class GameCollectibleFactoryTest {
         assertEquals(ItemType.BAG, bag.getType());
     }
 
+    /**
+     * Tests the creation of a hat item.
+     * <p>
+     * Verifies that:
+     * <ul>
+     * <li>The returned object is not null</li>
+     * <li>The item has the correct {@link ItemType#HAT} type</li>
+     * </ul>
+     * </p>
+     */
     @Test
     void testCreateHat() {
         final Collectible hat = factory.createHat(testPosition, testDimension);
         assertNotNull(hat);
         assertEquals(ItemType.HAT, hat.getType());
     }
-
 }
