8ce1f2a70ec2681ebce885df1d21bf82c939db19
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-11 13:25:01 +0200

    add: ladder interface

---
+++ src/main/java/it/unibo/coffebreak/model/api/entities/ladder/Ladder.java
@@ -0,0 +1,9 @@
+package it.unibo.coffebreak.model.api.entities.ladder;
+
+import it.unibo.coffebreak.model.api.entities.Entity;
+
+public interface Ladder extends Entity {
+
+    boolean isClimable();
+
+}
