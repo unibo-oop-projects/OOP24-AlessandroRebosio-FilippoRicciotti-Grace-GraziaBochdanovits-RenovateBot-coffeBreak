25812e7a6b59929dd72dbd946f2655ec3f2e3734
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-11 13:41:33 +0200

    docs: added JavaDoc for Collectible, Enenmy, Ladder and Platform

--- src/main/java/it/unibo/coffebreak/model/api/entities/ladder/Ladder.java
+++ src/main/java/it/unibo/coffebreak/model/api/entities/ladder/Ladder.java
@@ -2,8 +2,16 @@ package it.unibo.coffebreak.model.api.entities.ladder;
 
 import it.unibo.coffebreak.model.api.entities.Entity;
 
+/**
+ * Represents a ladder that the player can climb to move between platforms.
+ * 
+ * @author Alessandro Rebosio
+ */
 public interface Ladder extends Entity {
 
-    boolean isClimable();
-
+    /**
+     * @return true if this ladder can be currently climbed (e.g., not blocked by an
+     *         enemy)
+     */
+    boolean isClimbable();
 }
--- src/main/java/it/unibo/coffebreak/model/api/entities/platform/Platform.java
+++ src/main/java/it/unibo/coffebreak/model/api/entities/platform/Platform.java
@@ -2,15 +2,42 @@ package it.unibo.coffebreak.model.api.entities.platform;
 
 import it.unibo.coffebreak.model.api.entities.Entity;
 
+/**
+ * Represents a platform where entities can stand or move on.
+ * Platforms may have different slopes affecting movement.
+ * 
+ * @author Alessandro Rebosio
+ */
 public interface Platform extends Entity {
 
-    enum PlatformSlope {
+    /**
+     * Defines the slope direction of a platform.
+     */
+    enum Slope {
+        /**
+         * Platform slopes to the left, affecting movement speed/direction.
+         */
         LEFT,
+        /**
+         * Platform slopes to the right, affecting movement speed/direction.
+         */
         RIGHT,
+        /**
+         * Flat platform with no slope.
+         */
         FLAT
     }
 
-    PlatformSlope getSlope();
+    /**
+     * @return the slope of this platform
+     */
+    Slope getSlope();
 
+    /**
+     * Checks if this platform can support the given entity.
+     * 
+     * @param entity the entity to check
+     * @return true if the entity can stand on this platform, false otherwise
+     */
     boolean isSupporting(Entity entity);
 }
