40538a0041fd2414365ff6db3fd6d4cbe1b353e3
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-11 13:20:58 +0200

    docs: added JavaDoc for Model e GameModel

--- src/main/java/it/unibo/coffebreak/model/api/Model.java
+++ src/main/java/it/unibo/coffebreak/model/api/Model.java
@@ -7,11 +7,33 @@ import it.unibo.coffebreak.model.api.entities.character.Character;
 import it.unibo.coffebreak.model.api.score.Entry;
 import it.unibo.coffebreak.model.api.score.ScoreManager;
 
+/**
+ * Represents the main model interface for the Coffee Break game.
+ * The model acts as the central point for accessing game entities,
+ * player character, and score management functionality.
+ * 
+ * @author Alessandro Rebosio
+ */
 public interface Model {
 
+    /**
+     * Gets all entities currently present in the game world.
+     * 
+     * @return an unmodifiable list of all game entities
+     */
     List<Entity> getEntities();
 
+    /**
+     * Gets the player character.
+     * 
+     * @return the player character, or null if no player is set
+     */
     Character getPlayer();
 
+    /**
+     * Gets the score manager responsible for handling game scores.
+     * 
+     * @return the score manager instance
+     */
     ScoreManager<Entry> getScoreManager();
 }
--- src/main/java/it/unibo/coffebreak/model/impl/GameModel.java
+++ src/main/java/it/unibo/coffebreak/model/impl/GameModel.java
@@ -11,28 +11,48 @@ import it.unibo.coffebreak.model.api.score.Entry;
 import it.unibo.coffebreak.model.api.score.ScoreManager;
 import it.unibo.coffebreak.model.impl.score.GameScoreManager;
 
+/**
+ * A concrete implementation of the {@link Model}.
+ * This class maintains the game state including entities, player, and scores.
+ * 
+ * @author Alessandro Rebosio
+ */
 public class GameModel implements Model {
 
     private final List<Entity> entities;
     private final Character player;
     private final ScoreManager<Entry> scoreManager;
 
+    /**
+     * Constructs a new GameModel with empty entities list,
+     * no player character, and a new GameScoreManager instance.
+     */
     public GameModel() {
         this.entities = new ArrayList<>();
         this.scoreManager = new GameScoreManager();
         this.player = null;
     }
 
+    /**
+     * {@inheritDoc}
+     * The returned list is unmodifiable to maintain encapsulation.
+     */
     @Override
     public List<Entity> getEntities() {
         return Collections.unmodifiableList(this.entities);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Character getPlayer() {
         return this.player;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public ScoreManager<Entry> getScoreManager() {
         return this.scoreManager;
