48989cf08f1d81bcd4311f9bd3a1e7f70067f8ef
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-12 13:37:35 +0200

    docs: JavaDoc in Coin

--- src/main/java/it/unibo/coffebreak/model/impl/entities/collectible/Coin.java
+++ src/main/java/it/unibo/coffebreak/model/impl/entities/collectible/Coin.java
@@ -4,15 +4,37 @@ import it.unibo.coffebreak.model.api.entities.character.Character;
 import it.unibo.coffebreak.model.impl.util.Dimension2D;
 import it.unibo.coffebreak.model.impl.util.Position2D;
 
+/**
+ * Represents a coin collectible item in the game. When collected by a player
+ * character,
+ * it provides a certain value of points to the player's score.
+ * Extends {@link AbstractCollectible} to inherit basic collectible properties
+ * and behavior.
+ * 
+ * @author Alessandro Rebosio
+ */
 public class Coin extends AbstractCollectible {
 
+    /**
+     * Constructs a new Coin with the specified position, dimensions, and point
+     * value.
+     *
+     * @param position  the 2D position of the coin in the game world
+     * @param dimension the 2D dimensions (size) of the coin
+     * @param value     the point value this coin is worth when collected
+     */
     public Coin(final Position2D position, final Dimension2D dimension, final int value) {
         super(position, dimension, value);
     }
 
+    /**
+     * Applies the effect of collecting this coin to the player character.
+     * When collected, the coin adds its value to the player's score.
+     * 
+     * @param player the character that collected this coin
+     */
     @Override
     protected void applyEffect(final Character player) {
-        // TODO: player.getScoreManager().earnPoints(super.value);
+        // player.getScoreManager().earnPoints(super.value);
     }
-
 }
