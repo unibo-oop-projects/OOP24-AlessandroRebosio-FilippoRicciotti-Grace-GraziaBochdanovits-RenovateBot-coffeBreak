39af5f13051cad64eb54925ca7a1ac787cbfdd8f
Filippo Ricciotti <filippo.ricciotti@studio.unibo.it>
2025-05-12 18:23:09 +0200

    add: JavaDoc for Action enum

--- src/main/java/it/unibo/coffebreak/controller/handle/Action.java
+++ src/main/java/it/unibo/coffebreak/controller/handle/Action.java
@@ -1,12 +1,36 @@
 package it.unibo.coffebreak.controller.handle;
 
 public enum Action {
+    /**
+     * Action for starting the game.
+     */
     START,
+    /**
+     * Action for changing level.
+     */
     NEXT_LEVEL,
+    /**
+     * Action for resuming the gameplay.
+     */
     RESUME,
+    /**
+     * Action for quitting the game.
+     */
     EXIT_GAME,
+    /**
+     * Action for pausing the gameplay.
+     */
     PAUSE,
+    /**
+     * Action for moving left.
+     */
     MOVE_LEFT,
+    /**
+     * Action for moving up.
+     */
     MOVE_UP,
+    /**
+     * Action for moving right.
+     */
     MOVE_RIGHT
 }
