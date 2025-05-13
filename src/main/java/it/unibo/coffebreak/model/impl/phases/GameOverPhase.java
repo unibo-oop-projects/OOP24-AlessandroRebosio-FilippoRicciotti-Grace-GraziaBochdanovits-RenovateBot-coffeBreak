d45c54b039e530bcf1d533c11c3b3e3663ee452b
Filippo Ricciotti <filippo.ricciotti@studio.unibo.it>
2025-05-13 08:49:41 +0200

    add: implementation of GameOverPhase

---
+++ src/main/java/it/unibo/coffebreak/model/impl/phases/GameOverPhase.java
@@ -0,0 +1,53 @@
+package it.unibo.coffebreak.model.impl.phases;
+
+import it.unibo.coffebreak.controller.handle.Action;
+import it.unibo.coffebreak.model.api.Model;
+import it.unibo.coffebreak.model.api.phases.Phases;
+
+/**
+ * Implementation of {@link Phases} interface;
+ * <p>
+ * Represents the <b>Game Over</b> phase of the game.
+ * </p>
+ */
+public class GameOverPhase implements Phases {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void enterState() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void exitState() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void update(final long deltaTime) {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handleInput(final Action action, final Model model) {
+        switch (action) {
+            case Action.START:
+                model.setState(new InGamePhase());
+                break;
+            case Action.EXIT_GAME:
+                // TODO: exit the game
+                break;
+            default:
+                break;
+        }
+    }
+
+}
