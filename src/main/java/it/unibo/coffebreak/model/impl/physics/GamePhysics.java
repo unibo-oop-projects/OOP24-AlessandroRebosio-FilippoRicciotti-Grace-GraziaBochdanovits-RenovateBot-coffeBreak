eca054a34934d54758fb77e1beb35bc6e31864cc
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-14 00:36:19 +0200

    docs: javaDoc in Physic interface

--- src/main/java/it/unibo/coffebreak/model/impl/physics/GamePhysics.java
+++ src/main/java/it/unibo/coffebreak/model/impl/physics/GamePhysics.java
@@ -2,6 +2,21 @@ package it.unibo.coffebreak.model.impl.physics;
 
 import it.unibo.coffebreak.model.api.physics.Physics;
 
-public record GamePhysics(float acceleration, float maxSpeed, float deceleration, float gravity) implements Physics {
-
+/**
+ * Implementation of {@link Physics} that handles 2D game physics calculations.
+ * This record provides concrete physics computations for game entities
+ * including:
+ * <ul>
+ * <li>Movement with acceleration and friction</li>
+ * <li>Gravity application</li>
+ * </ul>
+ *
+ * <p>
+ * The physics system uses frame-rate independent calculations based on delta
+ * time.
+ * 
+ * @author Alessandro Rebosio
+ */
+public record GamePhysics() implements Physics {
+    // TODO: Implement physics methods
 }
