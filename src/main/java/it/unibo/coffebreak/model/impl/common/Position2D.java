985afc917ecfcd31981733cfff8e913811beb0fc
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-14 19:22:54 +0200

    fix: sum() in Position2D

--- src/main/java/it/unibo/coffebreak/model/impl/common/Position2D.java
+++ src/main/java/it/unibo/coffebreak/model/impl/common/Position2D.java
@@ -1,5 +1,7 @@
 package it.unibo.coffebreak.model.impl.common;
 
+import java.util.Objects;
+
 /**
  * Represents a position in a 2D space with x and y coordinates.
  *
@@ -12,11 +14,12 @@ public record Position2D(float x, float y) {
      * Sums this position with another position, creating a new Position2D instance
      * with coordinates equal to the sum of the corresponding coordinates.
      *
-     * @param position the position to add to this position
+     * @param vector the vector to add to this position
      * @return a new Position2D representing the sum of the two positions
      * @throws NullPointerException if the provided position is null
      */
-    public Position2D sum(final Position2D position) {
-        return new Position2D(this.x + position.x, this.y + position.y);
+    public Position2D sum(final Vector2D vector) {
+        Objects.requireNonNull(vector, "The vector cannot be null");
+        return new Position2D(this.x + vector.x(), this.y + vector.y());
     }
 }
