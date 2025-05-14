58341778a31155f5d03271b3233691b27acddd25
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-14 18:48:05 +0200

    add: method in Physics interface

--- src/main/java/it/unibo/coffebreak/model/api/physics/Physics.java
+++ src/main/java/it/unibo/coffebreak/model/api/physics/Physics.java
@@ -1,5 +1,8 @@
 package it.unibo.coffebreak.model.api.physics;
 
+import it.unibo.coffebreak.controller.api.command.Command;
+import it.unibo.coffebreak.model.impl.common.Vector2D;
+
 /**
  * Represents the physics system for game entities in a 2D space.
  * This interface defines the contract for physics calculations including:
@@ -7,5 +10,8 @@ package it.unibo.coffebreak.model.api.physics;
  * @author Alessandro Rebosio
  */
 public interface Physics {
-    // TODO: Implement physics methods
+
+    Vector2D calculateX(float deltaTme, Command command);
+
+    Vector2D calculateY(float deltaTme, Command command);
 }
