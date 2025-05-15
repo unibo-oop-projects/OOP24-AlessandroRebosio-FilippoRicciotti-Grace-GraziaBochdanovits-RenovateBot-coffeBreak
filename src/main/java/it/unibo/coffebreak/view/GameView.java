a1bef641aa98ee3d6fa18e1425918642e4102756
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-05-15 23:33:54 +0200

    docs: added JavaDoc in GameView

--- src/main/java/it/unibo/coffebreak/view/GameView.java
+++ src/main/java/it/unibo/coffebreak/view/GameView.java
@@ -1,31 +1,65 @@
 package it.unibo.coffebreak.view;
 
 import javax.swing.JPanel;
-
 import it.unibo.coffebreak.controller.api.Controller;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.Serial;
 
+/**
+ * The main game view component.
+ * 
+ * <p>
+ * This class extends {@link JPanel} and listens for keyboard input.
+ * Key events are passed to the game {@link Controller} for processing.
+ * </p>
+ * 
+ * <p>
+ * Part of the View in the MVC architecture.
+ * </p>
+ * 
+ * @author Alessandro Rebosio
+ */
 public class GameView extends JPanel implements KeyListener {
 
-    private final Controller controller;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-    public GameView(Controller controller) {
+    /** Reference to the game controller. */
+    private final transient Controller controller;
+
+    /**
+     * Constructs a GameView with the given controller.
+     *
+     * @param controller the controller to notify of key events
+     */
+    public GameView(final Controller controller) {
         this.controller = controller;
     }
 
+    /**
+     * Not used in this implementation.
+     */
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(final KeyEvent e) {
+        // Not used
     }
 
+    /**
+     * Sends the pressed key code to the controller.
+     *
+     * @param e the key event triggered when a key is pressed
+     */
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(final KeyEvent e) {
         this.controller.notifyInput(e.getKeyCode());
     }
 
+    /**
+     * Not used in this implementation.
+     */
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(final KeyEvent e) {
+        // Not used
     }
-
 }
