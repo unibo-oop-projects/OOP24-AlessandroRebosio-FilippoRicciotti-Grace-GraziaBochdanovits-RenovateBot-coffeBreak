a6e916d4cb9d1464f9c751ea79dc7d30f79acdec
Filippo Ricciotti <filippo.ricciotti@studio.unibo.it>
2025-05-15 17:06:30 +0200

    fix: removed PhaseType enum inside Phases interface

--- src/main/java/it/unibo/coffebreak/model/api/phases/Phases.java
+++ src/main/java/it/unibo/coffebreak/model/api/phases/Phases.java
@@ -9,28 +9,6 @@ import it.unibo.coffebreak.model.api.Model;
  * @author Filippo Ricciotti
  */
 public interface Phases {
-    /**
-     * Enum List of Game Phases.
-     */
-    enum PhaseType { // TODO: remove this enum is useless
-
-        /**
-         * Phasetype for when inside the menu.
-         */
-        MENU,
-        /**
-         * Phasetype for when playing the game.
-         */
-        IN_GAME,
-        /**
-         * Phasetype for when inside the paused menu.
-         */
-        PAUSE,
-        /**
-         * Phasetype for when inside the Game Over menu.
-         */
-        GAME_OVER
-    }
 
     /**
      * method for signaling the entrance in a new Phase.
@@ -48,7 +26,8 @@ public interface Phases {
      * @param action Input to handle.
      * @param model  the game model containing the possible Phase to change.
      */
-    void handleAction(Command action, Model model); // TODO: change param. name action to command, and rename it handleCommand
+    void handleAction(Command action, Model model); // TODO: change param. name action to command, and rename it
+                                                    // handleCommand
 
     /**
      * Updates the logic of the current game Phase based on deltaTime.
