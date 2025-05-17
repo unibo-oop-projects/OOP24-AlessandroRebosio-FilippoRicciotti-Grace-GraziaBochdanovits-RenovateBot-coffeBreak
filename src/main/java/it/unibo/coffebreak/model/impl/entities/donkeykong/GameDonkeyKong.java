678daa63e7fb8bcbac3d8b58c53a8c731181a770
Grace <grazia.bochdanovits@studio.unibo.it>
2025-05-17 12:35:54 +0200

    feat: add conditional throw to DonkeyKong interface

--- src/main/java/it/unibo/coffebreak/model/impl/entities/donkeykong/GameDonkeyKong.java
+++ src/main/java/it/unibo/coffebreak/model/impl/entities/donkeykong/GameDonkeyKong.java
@@ -1,10 +1,15 @@
 package it.unibo.coffebreak.model.impl.entities.donkeykong;
 
+import java.util.Optional;
+
 import it.unibo.coffebreak.model.api.entities.Entity;
 import it.unibo.coffebreak.model.api.entities.donkeykong.DonkeyKong;
+import it.unibo.coffebreak.model.api.entities.enemy.barrel.Barrel;
+import it.unibo.coffebreak.model.api.entities.enemy.barrel.BarrelFactory;
 import it.unibo.coffebreak.model.impl.common.Dimension2D;
 import it.unibo.coffebreak.model.impl.common.Position2D;
 import it.unibo.coffebreak.model.impl.entities.AbstractEntity;
+import it.unibo.coffebreak.model.impl.entities.enemy.barrel.GameBarrelFactory;
 
 /**
  * Implementation of the Donkey Kong enemy character.
@@ -30,6 +35,11 @@ public class GameDonkeyKong extends AbstractEntity implements DonkeyKong {
     private long lastThrowTime;
 
     /**
+     * The factory used to create new barrel instances.
+     */
+    private final BarrelFactory barrelFactory;
+
+    /**
      * Constructs a new Donkey Kong entity with specified position and dimensions.
      * 
      * @param position the initial position of Donkey Kong (cannot be null)
@@ -38,35 +48,54 @@ public class GameDonkeyKong extends AbstractEntity implements DonkeyKong {
      */
     public GameDonkeyKong(final Position2D position, final Dimension2D dimension) {
         super(position, dimension);
+        this.barrelFactory = new GameBarrelFactory();
     }
 
     /**
      * Updates Donkey Kong's state based on game time.
-     * <p>
-     * This implementation handles the automatic barrel throwing at fixed intervals.
-     * </p>
      * 
      * @param deltaTime the time elapsed since last update in milliseconds
      */
     @Override
     public void update(final float deltaTime) {
-        final long currentTime = System.currentTimeMillis();
-        if (currentTime - lastThrowTime >= BARREL_THROW_INTERVAL) {
-            throwBarrel();
-            lastThrowTime = currentTime;
-        }
+        // Intentionally empty
+    }
+
+     /**
+     * Creates and throws a new barrel from Donkey Kong's position.
+     * <p>
+     * Implementation details:
+     * <ul>
+     *   <li>Creates a basic barrel (non-fire variant)</li>
+     *   <li>Uses Donkey Kong's current position as starting point</li>
+     *   <li>Barrel velocity is set by the {@link GameBarrelFactory}</li>
+     * </ul>
+     * 
+     * @return the newly created {@link Barrel} instance
+     */
+    @Override
+    public Barrel throwBarrel() {
+        //TODO: calcola posizione che non sia proprio quella di DK
+        return barrelFactory.createBarrel(getPosition(), false);
     }
 
     /**
-     * Performs the barrel throwing action.
+     * Attempts to throw a barrel if the throw interval has elapsed.
      * <p>
-     * This method should create a new barrel entity and add it to the game world.
-     * </p>
+     * This method checks if {@link #BARREL_THROW_INTERVAL} has passed since the last throw.
+     * If so, it updates the throw timestamp and returns a new barrel wrapped in an Optional.
+     * Otherwise returns an empty Optional.
+     * 
+     * @return {@link Optional} containing a new {@link Barrel} if thrown, otherwise empty
      */
     @Override
-    public void throwBarrel() {
-        // TODO: Create barrel instance using a factory (?)
-        // TODO: Add barrel to game world through observer pattern (?)
+    public Optional<Barrel> tryThrowBarrel() {
+        final long currentTime = System.currentTimeMillis();
+        if (currentTime - lastThrowTime >= BARREL_THROW_INTERVAL) {
+            lastThrowTime = currentTime;
+            return Optional.of(throwBarrel());
+        }
+        return Optional.empty();
     }
 
     /**
