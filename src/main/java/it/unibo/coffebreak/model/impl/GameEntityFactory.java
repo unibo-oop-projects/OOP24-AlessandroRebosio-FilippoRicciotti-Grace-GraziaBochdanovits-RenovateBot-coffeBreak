b4dc7b4d902e358202c3cb950b94e088ebc02d5d
Filippo Ricciotti <filippo.ricciotti@studio.unibo.it>
2025-05-23 12:19:27 +0200

    add: raw implemtation of EntityFactory

---
+++ src/main/java/it/unibo/coffebreak/model/impl/GameEntityFactory.java
@@ -0,0 +1,118 @@
+package it.unibo.coffebreak.model.impl;
+
+import it.unibo.coffebreak.model.api.EntityFactory;
+import it.unibo.coffebreak.model.api.entities.Entity;
+import it.unibo.coffebreak.model.impl.common.Position2D;
+import it.unibo.coffebreak.model.impl.entities.collectible.coin.Coin;
+import it.unibo.coffebreak.model.impl.entities.collectible.hammer.Hammer;
+import it.unibo.coffebreak.model.impl.entities.enemy.barrel.GameBarrel;
+import it.unibo.coffebreak.model.impl.entities.enemy.fire.GameFire;
+import it.unibo.coffebreak.model.impl.entities.mario.Mario;
+import it.unibo.coffebreak.model.impl.entities.npc.donkeykong.DonkeyKong;
+import it.unibo.coffebreak.model.impl.entities.npc.princess.Princess;
+import it.unibo.coffebreak.model.impl.entities.structure.ladder.normal.NormalLadder;
+import it.unibo.coffebreak.model.impl.entities.structure.platform.breakable.BreakablePlatform;
+import it.unibo.coffebreak.model.impl.entities.structure.platform.normal.NormalPlatform;
+import it.unibo.coffebreak.model.impl.entities.structure.tank.GameTank;
+
+/**
+ * {@code GameEntityFactory} is the implementation of the
+ * <b>{@link EntityFactory} interface</b>
+ * <p>
+ * It provides the logic for instantiating
+ * all the different types of game entities
+ * </p>
+ * 
+ * @author Filippo Ricciotti
+ */
+public class GameEntityFactory implements EntityFactory {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createCoin(final Position2D position) {
+        return new Coin(position, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createHammer(final Position2D position) {
+        return new Hammer(position, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createBarrel(final Position2D position) {
+        return new GameBarrel(position, null, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createFire(final Position2D position) {
+        return new GameFire(position, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createDonkeyKong(final Position2D position) {
+        return new DonkeyKong(position, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createPrincess(final Position2D position) {
+        return new Princess(position, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createLadder(final Position2D position) {
+        return new NormalLadder(position, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createPlatform(final Position2D position) {
+        return new NormalPlatform(position, null, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createBreakablePlatform(final Position2D position) {
+        return new BreakablePlatform(position, null, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createTank(final Position2D position) {
+        return new GameTank(position, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Entity createMario(final Position2D position) {
+        return new Mario(position, null);
+    }
+
+}
