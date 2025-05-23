7cab3c1faf63c51c3af079a0285222aec76b7604
Filippo Ricciotti <filippo.ricciotti@studio.unibo.it>
2025-05-23 13:52:10 +0200

    add: partial implementation of loadEntities

--- src/main/java/it/unibo/coffebreak/model/impl/level/GameLevelManager.java
+++ src/main/java/it/unibo/coffebreak/model/impl/level/GameLevelManager.java
@@ -1,5 +1,8 @@
 package it.unibo.coffebreak.model.impl.level;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -7,6 +10,7 @@ import java.util.Objects;
 import it.unibo.coffebreak.model.api.entities.Entity;
 import it.unibo.coffebreak.model.api.level.LevelManager;
 import it.unibo.coffebreak.model.api.level.cleaner.Cleaner;
+import it.unibo.coffebreak.model.impl.common.Position2D;
 import it.unibo.coffebreak.model.impl.level.cleaner.EntityCleaner;
 
 /**
@@ -19,6 +23,8 @@ public class GameLevelManager implements LevelManager {
 
     private final List<Entity> entities;
     private final Cleaner cleanup;
+    private static final String PATH_MAP1 = "maps/Map1.txt";
+    private static final int P_SIZE = 10;
 
     /**
      * Constructs a new {@code GameLevelManager} with an empty entity list
@@ -39,6 +45,38 @@ public class GameLevelManager implements LevelManager {
     @Override
     public void loadEntities() {
         // TODO: load entities from a file or level descriptor
+        try {
+            List<String> lines = Files.readAllLines(Path.of(PATH_MAP1));
+            for (int row = 0; row < lines.size(); row++) {
+                String line = lines.get(row);
+                for (int col = 0; col < line.length(); col++) {
+                    char ch = line.charAt(col);
+                    float x = col * P_SIZE;
+                    float y = row * P_SIZE;
+                    Position2D pos = new Position2D(x, y);
+
+                    switch (ch) {// TODO: use Factory
+                        case 'P' -> entities.add(null);
+                        case 'L' -> entities.add(null);
+                        case 'M' -> entities.add(null);
+                        case 'D' -> entities.add(null);
+                        case 'Q' -> entities.add(null);
+                        case 'B' -> entities.add(null);
+                        case 'F' -> entities.add(null);
+                        case 'C' -> entities.add(null);
+                        case 'H' -> entities.add(null);
+                        case 'T' -> entities.add(null);
+
+                        default -> {
+                            /* ignorato */ }
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**
