6048fcd472092894ea6f01797a7f572bb493740f
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-06-10 17:41:28 +0200

    fix: indent tab on EntityRender

--- src/main/java/it/unibo/coffebreak/api/view/render/entities/EntityRender.java
+++ src/main/java/it/unibo/coffebreak/api/view/render/entities/EntityRender.java
@@ -6,20 +6,21 @@ import it.unibo.coffebreak.api.model.entities.Entity;
 
 /**
  * An interface that defines a renderer for a specific type of game entity.
- * Implementations of this interface are responsible for defining how a particular
+ * Implementations of this interface are responsible for defining how a
+ * particular
  * type of entity should be rendered on screen.
  * 
  * @author Grazia Bochdanovits de Kavna
  */
 public interface EntityRender {
-   /**
-     * Renders the specified entity using the provided graphics context.
-     * Implementations should define the visual representation of the entity
-     * by drawing appropriate graphics elements.
-     *
-     * @param g         the Graphics2D context used for rendering
-     * @param entity    the entity to be rendered
-     * @param deltaTime the time passed since the last frame
-     */
-    void draw(Graphics2D g, Entity entity, float deltaTime);
+	/**
+	 * Renders the specified entity using the provided graphics context.
+	 * Implementations should define the visual representation of the entity
+	 * by drawing appropriate graphics elements.
+	 *
+	 * @param g         the Graphics2D context used for rendering
+	 * @param entity    the entity to be rendered
+	 * @param deltaTime the time passed since the last frame
+	 */
+	void draw(Graphics2D g, Entity entity, float deltaTime);
 }
