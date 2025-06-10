ee50fb8660b49843defc2ffec3d2ec0c3f9f1cbd
Grazia Bochdanovits <grazia.bochdanovits@studio.unibo.it>
2025-06-10 19:16:43 +0200

    add: donkeyKongRender impl

---
+++ src/main/java/it/unibo/coffebreak/impl/view/renders/entities/donkeykong/DonkeyKongRender.java
@@ -0,0 +1,44 @@
+package it.unibo.coffebreak.impl.view.renders.entities.donkeykong;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import it.unibo.coffebreak.api.model.entities.Entity;
+import it.unibo.coffebreak.api.view.loader.Loader;
+import it.unibo.coffebreak.impl.model.entities.npc.donkeykong.DonkeyKong;
+import it.unibo.coffebreak.impl.view.renders.entities.AbstractEntityRender;
+
+/**
+ * A renderer for ladder entities that draws them as red rectangles on the
+ * screen.
+ * This class extends {@link AbstractScalableRender} to provide scaling
+ * functionality
+ * based on the screen dimensions.
+ * 
+ * @author Grazia Bochdanovits de Kavna
+ */
+public class DonkeyKongRender extends AbstractEntityRender {
+
+    /**
+     * Constructs a new LadderRender with the specified screen dimensions.
+     * The ladder dimensions will be scaled according to these dimensions.
+     *
+     * @param resource
+     */
+    public DonkeyKongRender(final Loader resource) {
+        super(resource);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void draw(final Graphics2D g, final Entity entity, final float deltaTime) {
+        if (entity instanceof DonkeyKong) {
+            g.setColor(Color.YELLOW);
+
+            g.drawRect((int) entity.getPosition().x(), (int) entity.getPosition().y(),
+                    (int) entity.getDimension().width(), (int) entity.getDimension().height());
+        }
+    }
+}
