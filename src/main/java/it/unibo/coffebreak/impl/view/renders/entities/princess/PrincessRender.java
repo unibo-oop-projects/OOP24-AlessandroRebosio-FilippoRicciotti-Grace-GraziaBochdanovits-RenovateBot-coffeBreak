2f286e3a7cb30e15faecd309473e77993b600d68
Grazia Bochdanovits <grazia.bochdanovits@studio.unibo.it>
2025-06-10 19:22:54 +0200

    add: princessRender impl

--- src/main/java/it/unibo/coffebreak/impl/view/renders/entities/princess/PrincessRender.java
+++ src/main/java/it/unibo/coffebreak/impl/view/renders/entities/princess/PrincessRender.java
@@ -1,38 +1,38 @@
-package it.unibo.coffebreak.impl.view.renders.entities.donkeykong;
+package it.unibo.coffebreak.impl.view.renders.entities.princess;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
 
 import it.unibo.coffebreak.api.model.entities.Entity;
+import it.unibo.coffebreak.api.model.entities.npc.Princess;
 import it.unibo.coffebreak.api.view.loader.Loader;
-import it.unibo.coffebreak.impl.model.entities.npc.donkeykong.DonkeyKong;
 import it.unibo.coffebreak.impl.view.renders.entities.AbstractEntityRender;
 
 /**
- * A renderer for DonkeyKong that draws him as a yellow rectangle on the
+ * A renderer for the princess that draws him as a pink rectangle on the
  * screen.
  * 
  * @author Grazia Bochdanovits de Kavna
  */
-public class DonkeyKongRender extends AbstractEntityRender {
+public class PrincessRender extends AbstractEntityRender {
 
     /**
-     * Constructs a new DonkeyKong with the specified screen dimensions.
-     * The entity dimensions will be scaled according to these dimensions.
+     * Constructs a new Princess with the specified screen dimensions.
+     * The target dimensions will be scaled according to these dimensions.
      *
      * @param resource
      */
-    public DonkeyKongRender(final Loader resource) {
+    public PrincessRender(final Loader resource) {
         super(resource);
     }
 
-    /**
+   /**
      * {@inheritDoc}
      */
     @Override
     public void draw(final Graphics2D g, final Entity entity, final float deltaTime) {
-        if (entity instanceof DonkeyKong) {
-            g.setColor(Color.YELLOW);
+        if (entity instanceof Princess) {
+            g.setColor(Color.PINK);
 
             g.drawRect((int) entity.getPosition().x(), (int) entity.getPosition().y(),
                     (int) entity.getDimension().width(), (int) entity.getDimension().height());
