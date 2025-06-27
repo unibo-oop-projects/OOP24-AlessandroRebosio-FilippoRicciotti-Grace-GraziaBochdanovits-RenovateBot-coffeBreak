1c459d4a0180454407c2082ce41be22172e17857
Grazia Bochdanovits <grazia.bochdanovits@studio.unibo.it>
2025-06-27 20:28:36 +0200

    add tests for platform

--- src/test/java/it/unibo/coffebreak/model/entities/structure/tank/TestGameTank.java
+++ src/test/java/it/unibo/coffebreak/model/entities/structure/tank/TestGameTank.java
@@ -1,4 +1,4 @@
-package it.unibo.coffebreak.model.entities.tank;
+package it.unibo.coffebreak.model.entities.structure.tank;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
